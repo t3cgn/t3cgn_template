fbf93547587cef8c6461f31792401a9727736bea
Vanessa Kestering <mail@vanessakestering.de>
2017-02-13 20:51:07 +0100

    [TASK] Prepares parallax layers for homepage and z-index ordering

--- Configuration/TypoScript/Setup/23.Page.Scripts.ts
+++ Configuration/TypoScript/Setup/23.Page.Scripts.ts
@@ -7,6 +7,9 @@ page {
     includeJSFooterlibs  {
         jQuery = EXT:t3cgn_template/Resources/Public/JavaScript/Vendor/jquery-3.1.1.min.js
         bootstrap = EXT:t3cgn_template/Resources/Private/Design/Vendor/bootstrap/dist/js/bootstrap.min.js
+
+        parallaxHomepage = EXT:t3cgn_template/Resources/Public/JavaScript/Parallax/parallaxHomepage.js
+        mainJs = EXT:t3cgn_template/Resources/Public/JavaScript/main.js
     }
 
 }
---
+++ Resources/Public/JavaScript/Parallax/parallaxHomepage.js
@@ -0,0 +1,8 @@
+var parallaxHomepage = function () {
+
+    var parallaxLayer = $('#mergedParallax');
+    var ratio = 5.03;
+
+    parallaxLayer.height(parallaxLayer.height() * ratio);
+
+};
