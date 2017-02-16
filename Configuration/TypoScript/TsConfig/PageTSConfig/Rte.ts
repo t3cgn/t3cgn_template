52d0993a9596769626227ffc2ffc13b6ffb9f3c6
Vanessa Kestering <mail@vanessakestering.de>
2017-02-16 22:52:33 +0100

    [TASK] Adds rte.ts and styles for rich text editor

---
+++ Configuration/TypoScript/TsConfig/PageTSConfig/Rte.ts
@@ -0,0 +1,33 @@
+RTE {
+    default {
+        useCSS = 1
+        contentCSS = typo3conf/ext/t3cgn_template/Resources/Public/Css/rte.css
+        proc {
+            allowedClasses (
+              external-link, external-link-new-window, internal-link, internal-link-new-window, download, mail, btn-primary, btn-default
+            )
+            allowedClasses := addToList(btn-primary, btn-default)
+        }
+
+        buttons {
+            link.properties.class.allowedClasses := addToList(btn-primary, btn-default)
+        }
+
+
+        ## nicht benoetigte Klassen entfernen
+        #RTE.default.proc.allowedClasses := removeFromList(csc-frame-frame1, csc-frame-frame2, important, name-of-person, detail)
+        buttons {
+            RTE.classesAnchor.btn-primary {
+                name = Button (border orange)
+                value =
+            }
+        }
+
+        classesAnchor.default {
+            page = btn-default
+            url = external-link-new-window
+            file = download
+            mail = mail
+        }
+   }
+}
