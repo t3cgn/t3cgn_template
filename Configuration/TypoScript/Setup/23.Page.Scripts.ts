f086269b9504941808c505c24bc70c6d51692d22
Vanessa <mail@vanessakestering.de>
2017-02-18 21:49:40 +0100

    [TASK] Adds jQuery tether

--- Configuration/TypoScript/Setup/23.Page.Scripts.ts
+++ Configuration/TypoScript/Setup/23.Page.Scripts.ts
@@ -6,10 +6,12 @@ page {
 
     includeJSFooterlibs  {
         jQuery = EXT:t3cgn_template/Resources/Public/JavaScript/Vendor/jquery-3.1.1.min.js
+        tether = EXT:t3cgn_template/Resources/Public/JavaScript/Vendor/tether.min.js
         bootstrap = EXT:t3cgn_template/Resources/Private/Design/Vendor/bootstrap/dist/js/bootstrap.min.js
 
         parallaxHomepage = EXT:t3cgn_template/Resources/Public/JavaScript/Parallax/parallaxHomepage.js
         mainNavigation = EXT:t3cgn_template/Resources/Public/JavaScript/mainNavigation.js
+        accordionTrigger = EXT:t3cgn_template/Resources/Public/JavaScript/accordionTrigger.js
 
         mainJs = EXT:t3cgn_template/Resources/Public/JavaScript/main.js
     }
