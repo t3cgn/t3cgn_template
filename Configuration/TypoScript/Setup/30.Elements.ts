bfdadbe5feb0b0bb068c3d5e6ab1c9530aa5d853
Felix Rauch <rauch@skaiamail.de>
2017-02-18 21:49:34 +0100

    [FIX] Accidentally overrode FSC default layout

--- Configuration/TypoScript/Setup/30.Elements.ts
+++ Configuration/TypoScript/Setup/30.Elements.ts
@@ -1,7 +1,7 @@
 lib.fluidContent {
-    templateRootPaths.4711 = EXT:t3cgn_template/Resources/Private/Templates/Content
-    partialRootPaths.4711  = EXT:t3cgn_template/Resources/Private/Partials
-    layoutRootPaths.4711   = EXT:t3cgn_template/Resources/Private/Layouts
+    templateRootPaths.4711 = EXT:t3cgn_template/Resources/Private/Templates/Content/
+    partialRootPaths.4711  = EXT:t3cgn_template/Resources/Private/Partials/Content/
+    layoutRootPaths.4711   = EXT:t3cgn_template/Resources/Private/Layouts/Content/
 }
 
 tt_content {
